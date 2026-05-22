308754142fb048713b630aa6a775cc8c31e48d59
ubuntu <jjonah322@gmail.com>
2026-05-22 16:23:50 +0330

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test-ai', function () {
-    $gpt = new \App\Actions\GptAction("test");
 
-    $res = $gpt->ask("یک متن کوتاه درباره Laravel بنویس");
-
-    dd($res);
-});
