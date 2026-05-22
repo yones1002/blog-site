c6bba8292b7dbf797912fa1711a99571cb021ee3
Younes Sahraei <jjonah322@gmail.com>
2026-05-22 16:37:07 +0330

    feat:add console route

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
     $this::command('app:import-news-command')->hourly();
+    $this::command('app:faq-generate')->hourly();
+    $this::command('app:seo-generate')->hourly();
 })->purpose('Display an inspiring quote');
